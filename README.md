96a65cab241a79e77f0562444fa95af20a41ed92
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-08-31 14:17:20 +0200

    uppgift1.4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # prog2-ovn
  övningar för programmering 1
+
+detta är ett övningsrepo för kursen programmering 1.
---
+++ uppgift1.4.py
@@ -0,0 +1,6 @@
+# Uppgift1.4.py
+
+name = input (' vad heter du? ')
+efternam = input ('efternam? ')
+ålder = input ('hur gammal är du? ')
+print = ('Hej', name, efternam, ålder) 
