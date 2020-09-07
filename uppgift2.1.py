620679b3958c19d499abc6fde128bcc5474665ed
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-09-07 13:24:29 +0200

    uppgift2.1.py

--- uppgift2.1.py
+++ uppgift2.1.py
@@ -1,2 +1,11 @@
+name = input ('vad heter du? ')
+surname = input ('Efternamn? ')
+Age = input ('Ålder? ')
+Sex = input ('Gender? ')
+print ('Welcome Sir!', name,' ', surname, ' asdf', Age, ' ', Sex)
+print ('You V class is')
+
 a, b, c = 12, 12.0, "12.0"
 print (type(a), type(b), type(c))
+
+
