e458caab1f80b4651e7e0236fd1f096ec68a79dc
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-09-07 14:36:45 +0200

    uppgfit2.3.py

---
+++ uppgift2.3.py
@@ -0,0 +1,14 @@
+svar = input (" Tall i dicemaltal!: ")
+x = float (svar)
+y = x*x
+print(f'Talet i kvatrat är {y:.2f}')
+print(f'result: {y}')
+print(f'result: {y:20}')
+print(f'result: {y:09}')
+
+svar = input (" Tall i heltal!: ")
+x = int (svar)
+y = x*x
+print(f'Talet i kvatrat är {y}')
+print(f'result: {y}')
+
