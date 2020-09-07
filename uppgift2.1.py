b17df86913b5a871c4d5677586ee18ba66a3a908
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-09-07 13:29:57 +0200

    uppgfit2.1.py

--- uppgift2.1.py
+++ uppgift2.1.py
@@ -1,11 +1,4 @@
-name = input ('vad heter du? ')
-surname = input ('Efternamn? ')
-Age = input ('Ålder? ')
-Sex = input ('Gender? ')
-print ('Welcome Sir!', name,' ', surname, ' asdf', Age, ' ', Sex)
-print ('You V class is')
-
-a, b, c = 12, 12.0, "12.0"
+a, b, c = 2, 12.0, "12.0"
 print (type(a), type(b), type(c))
 
 
