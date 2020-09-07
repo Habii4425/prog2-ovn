886a56ec5f790f890b5b0d7c184dac334fd6e10b
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-09-07 14:54:45 +0200

    uppgift2.4.py

---
+++ uppgift2.4.py
@@ -0,0 +1,7 @@
+svar = input('hur lång är kvadraten? ')
+i = float(svar)
+omkrets = i+i+i+i
+Arean =  i*i
+
+print(f'Kvadratens omkrets är: {omkrets:10.2f}')
+print(f'Kvadratens Arean är: {Arean:10.2f}')
