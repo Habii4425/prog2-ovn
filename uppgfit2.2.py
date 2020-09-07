e76878f396b769d211b5a8c7f4cded6ee0a5f274
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-09-07 13:42:09 +0200

    uppgift2.2.py

---
+++ uppgfit2.2.py
@@ -0,0 +1,9 @@
+svar = input('skriv ett tal: ')
+x = float(svar)
+y = x * x
+print (' Here you go!', y)
+
+svar1 = input('skriv ett tal till: ')
+x1 = int(svar1)
+y1 = x1*x1
+print ('Here you go bro!', y1)
