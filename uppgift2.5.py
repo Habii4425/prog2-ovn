93855787e5df9e34e3f4f0c4a2f9a53cbccdca25
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-10-19 14:08:29 +0200

    uppgift2.5.py

--- uppgift2.5.py
+++ uppgift2.5.py
@@ -1,6 +1,5 @@
 svar = input('Pris på varan?  ')
-i = float(svar)
-ink = i * 1.15
-exk = i * 0.85
-print(f'Totalt pris inklusiv moms {ink:4.2f}')
-print(f'Totalt pris exklusiv moms {exk:4.2f}')
+x = int(svar)
+m = 0.75
+p = x*m
+print('priset med moms',x, 'priset utan moms: ',p)
