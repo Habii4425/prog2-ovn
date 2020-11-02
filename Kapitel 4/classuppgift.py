ebd57c7cacb5630fb63640b84a752f97d68f12d3
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-11-02 14:02:00 +0100

    Update classuppgift.py

--- Kapitel 4/classuppgift.py
+++ Kapitel 4/classuppgift.py
@@ -1,13 +1,18 @@
 import random
 
-guess = 0
 correct = random.randint(1, 20)
+guasses = 1
+
+guess = 0
 while guess != correct:
-    guess = int(input("Gissa på ett tall: "))
+    guasses += 1
+    guess = int(input("Gissa på ett tall mellan 1 och 20: "))
     
     if guess < correct:
       print(f"för litet")
     elif guess > correct:
       print(f"för stort")
+
+    
 
 print("Right Guess!")
