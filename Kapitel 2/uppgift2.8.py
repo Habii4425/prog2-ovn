b8c5d9a4a953b1f2100d06c03a67ae354b7795c8
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-11-02 14:14:30 +0100

    Update uppgift2.8.py

---
+++ Kapitel 2/uppgift2.8.py
@@ -0,0 +1,9 @@
+import random
+
+print('Träningarna är kastad!')
+n = random.randint(1,6)
+n2 = random.randint(1,6)
+summan = n+n2
+print('Du fick','[', n,']','på ena tärningen')
+print('Du fick','[', n2,']','på andra tärningen')
+print('Totalt: ', summan)
