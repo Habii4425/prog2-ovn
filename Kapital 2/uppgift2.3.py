6a54838c6113448862a36d343d00e4308d6037bf
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-11-02 14:32:02 +0100

    Create uppgift2.3.py

---
+++ Kapital 2/uppgift2.3.py
@@ -0,0 +1,8 @@
+svar = input (" Tall i dicemaltal!: ")
+x = float (svar)
+y = x*x
+print(f'Talet i kvatrat är {y:.2f}')
+print(f'result: {y}')
+print(f'result: {y:20}')
+print(f'result: {y:09}')
+print(f'y={y}')
