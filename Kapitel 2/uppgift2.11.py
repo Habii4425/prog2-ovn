13426e7096397564a9b651f6f5e2cd8a95625fa6
Habii4425 <habibullah.haidari@elev.ga.ntig.se>
2020-11-02 14:31:35 +0100

    Delete uppgift2.11.py

--- Kapitel 2/uppgift2.11.py
+++
@@ -1,8 +0,0 @@
-import math
-
-r = float(input ('Skriv ett tall:  '))
-V = 4 * math.pi * r * r * r / 3
-A = 4 * math.pi * r * r
-
-print (f'Talets volymen är: {V:4.2f}')
-print (f'Talets arean är: {A:4.2f}')
